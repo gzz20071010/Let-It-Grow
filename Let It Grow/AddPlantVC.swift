dc5fe7b9ae03eccf7ce919939b5484eeecfe6144
shengxiangguo <sg3214@nyu.edu>
2016-02-12 04:11:16 -0500

    added addPlantVC

---
+++ Let It Grow/AddPlantVC.swift
@@ -0,0 +1,21 @@
+//
+//  AddPlantVC.swift
+//  Let It Grow
+//
+//  Created by shengxiang guo on 2/12/16.
+//  Copyright © 2016 shengxiang guo. All rights reserved.
+//
+
+import UIKit
+
+class AddPlantVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
